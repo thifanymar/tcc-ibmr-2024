b37234f854a0d7825b5862c73b6af56d29d86030
thifany <thifanymar@gmail.com>
2024-10-19 17:12:02 -0300

    projeto completo

--- modelo.py
+++ modelo.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+import os
 
 # lendo os arquivos 
 df_venda = pd.read_excel(r'files\venda.xlsx')
@@ -13,12 +16,35 @@ df_empreendimento = pd.read_excel(r'files\empreendimento.xlsx')
 
 
 # TODO: colocar tudo em string menos data e valor de venda, fgts, renda, financiamento
+colunas = ['num_venda','num_empreendimento']
+df_venda[colunas] = df_venda[colunas].astype(str)
 
+colunas = ['num_usuario', 'nome_usuario', 'cpf_usuario']
+df_usuario[colunas] = df_usuario[colunas].astype(str)
+
+colunas = ['num_venda', 'num_corretor', 'num_equipe']
+df_corretor_venda[colunas] = df_corretor_venda[colunas].astype(str)
+
+colunas = ['num_equipe', 'num_gerente', 'num_diretor']
+df_equipe[colunas] = df_equipe[colunas].astype(str)
+
+colunas = ['num_cliente', 'nome', 'bairro', 'cep', 'profissao', 'escolaridade','uf', 'estado_civil']
+df_cliente_crm[colunas] = df_cliente_crm[colunas].astype(str)
+
+colunas = ['num_cliente','num_venda']
+df_cliente_venda[colunas] = df_cliente_venda[colunas].astype(str)
+
+colunas = ['num_incorporador', 'nome_incorporador']
+df_incorporador[colunas] = df_incorporador[colunas].astype(str)
+
+colunas = ['num_empreendimento', 'num_incorporador', 'nome_empreendimeto','cep_empreendimento', 'bairro_empreendimento', 'uf_empreendimento']
+df_empreendimento[colunas] = df_empreendimento[colunas].astype(str)
 
 # montar tabela fato venda
 fVenda = pd.merge(df_venda,df_corretor_venda,how='right',left_on='num_venda',right_on='num_venda')
-fVenda = fVenda.drop(columns=['num_equipe'])
-
+fVenda = fVenda.drop(columns=['num_corretor'])
+fVenda = pd.merge(fVenda,df_cliente_venda,how='right',left_on='num_venda',right_on='num_venda')
+fVenda = fVenda.drop(columns=['valor_fgts','valor_renda','valor_financiamento'])
 # montar tabela cliente
 dCliente = pd.merge(df_cliente_crm,df_cliente_venda,how='right',left_on='num_cliente',right_on='num_cliente')
 
@@ -67,5 +93,18 @@ dCalendario = pd.DataFrame({
 })
 
 print(dCalendario)
+
+
+load_dotenv()
+database_url = os.getenv('db_url')
+engine = create_engine(database_url)
+
+fVenda.to_sql('fvenda', con=engine, if_exists='replace', index=False)
+dEquipe.to_sql('dequipe', con=engine, if_exists='replace', index=False)
+dProduto.to_sql('dproduto', con=engine, if_exists='replace', index=False)
+dCliente.to_sql('dcliente', con=engine, if_exists='replace', index=False)
+dCalendario.to_sql('dcalendario', con=engine, if_exists='replace', index=False)
+
+engine.dispose()
 
 # salvar os dados em um banco de dados
