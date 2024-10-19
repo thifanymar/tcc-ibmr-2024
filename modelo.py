689449dd4d97008f2e5a44d2f527e73a4e97dce8
thifany <thifanymar@gmail.com>
2024-10-19 20:01:59 -0300

    ajuste tabela calendario

--- modelo.py
+++ modelo.py
@@ -16,33 +16,70 @@ df_empreendimento = pd.read_excel(r'files\empreendimento.xlsx')
 
 
 # TODO: colocar tudo em string menos data e valor de venda, fgts, renda, financiamento
+
+# ajustes tabela venda
 colunas = ['num_venda','num_empreendimento']
 df_venda[colunas] = df_venda[colunas].astype(str)
+df_venda = df_venda.rename({'num_empreendimento':'num_produto',
+                            'data':'data_venda'}, axis='columns')
+df_venda['data_venda'] = df_venda['data_venda'].apply(pd.to_datetime, format='%d/%m/%Y')
+df_venda['valor'] = df_venda['valor'].astype(float)
 
+# ajustes tabela usuario
 colunas = ['num_usuario', 'nome_usuario', 'cpf_usuario']
 df_usuario[colunas] = df_usuario[colunas].astype(str)
+df_usuario['data_nascimento'] = df_usuario['data_nascimento'].apply(pd.to_datetime, format='%d/%m/%Y')
+colunas = ['nome_usuario']
+df_usuario[colunas] = df_usuario[colunas].apply(lambda x: x.str.title())
 
+# ajustes tabela corretor_venda
 colunas = ['num_venda', 'num_corretor', 'num_equipe']
 df_corretor_venda[colunas] = df_corretor_venda[colunas].astype(str)
 
+# ajustes tabela equipe
 colunas = ['num_equipe', 'num_gerente', 'num_diretor']
 df_equipe[colunas] = df_equipe[colunas].astype(str)
 
+# ajustes tabela cliente_crm
 colunas = ['num_cliente', 'nome', 'bairro', 'cep', 'profissao', 'escolaridade','uf', 'estado_civil']
 df_cliente_crm[colunas] = df_cliente_crm[colunas].astype(str)
+df_cliente_crm = df_cliente_crm.rename({'nome':'nome_cliente',
+                                        'bairro':'bairro_cliente',
+                                        'profissao':'profissao_cliente',
+                                        'escolaridade':'escolaridade_cliente',
+                                        'estado_civil':'estado_civil_cliente'}, axis='columns')
 
+colunas = ['nome_cliente', 'bairro_cliente','profissao_cliente', 'escolaridade_cliente','uf', 'estado_civil_cliente']
+df_cliente_crm[colunas] = df_cliente_crm[colunas].apply(lambda x: x.str.title())
+
+# ajustes tabela cliente_venda
 colunas = ['num_cliente','num_venda']
 df_cliente_venda[colunas] = df_cliente_venda[colunas].astype(str)
+colunas = ['valor_fgts','valor_renda','valor_financiamento']
+df_cliente_venda[colunas] = df_cliente_venda[colunas].astype(float)
 
+# ajustes tabela incorporador
 colunas = ['num_incorporador', 'nome_incorporador']
 df_incorporador[colunas] = df_incorporador[colunas].astype(str)
+df_incorporador = df_incorporador.rename({'num_incorporador':'num_construtora',
+                                          'nome_incorporador':'nome_construtora'}, axis='columns')
 
+colunas = ['nome_construtora']
+df_incorporador[colunas] = df_incorporador[colunas].apply(lambda x: x.str.title())
+
+# ajustes tabela empreendimento
 colunas = ['num_empreendimento', 'num_incorporador', 'nome_empreendimeto','cep_empreendimento', 'bairro_empreendimento', 'uf_empreendimento']
 df_empreendimento[colunas] = df_empreendimento[colunas].astype(str)
+df_empreendimento = df_empreendimento.rename({'num_empreendimento':'num_produto',
+                                              'nome_empreendimeto':'nome_produto',
+                                              'num_incorporador':'num_construtora'}, axis='columns')
+
+colunas = ['nome_produto']
+df_empreendimento[colunas] = df_empreendimento[colunas].apply(lambda x: x.str.title())
 
 # montar tabela fato venda
 fVenda = pd.merge(df_venda,df_corretor_venda,how='right',left_on='num_venda',right_on='num_venda')
-fVenda = fVenda.drop(columns=['num_corretor'])
+
 fVenda = pd.merge(fVenda,df_cliente_venda,how='right',left_on='num_venda',right_on='num_venda')
 fVenda = fVenda.drop(columns=['valor_fgts','valor_renda','valor_financiamento'])
 # montar tabela cliente
@@ -73,15 +110,19 @@ dEquipe = dEquipe.drop(columns=['data_nascimento_gerente',
                                 'cpf_diretor'])
 
 # montar tabela produto
-dProduto = pd.merge(df_empreendimento,df_incorporador,how='left',left_on='num_incorporador',right_on='num_incorporador')
+dProduto = pd.merge(df_empreendimento,df_incorporador,how='left',left_on='num_construtora',right_on='num_construtora')
 
 # montar tabela calendario
 
 dCalendario = ''
-data_inicio = '2024-01-01'
-data_fim = '2024-12-31'
 
-datas = pd.date_range(start=data_inicio, end=data_fim, freq='D')
+data_max = fVenda['data_venda'].max().date()
+data_max_str = data_max.strftime('%Y-%m-%d')
+
+data_min = fVenda['data_venda'].min().date()
+data_min_str = data_min.strftime('%Y-%m-%d')
+
+datas = pd.date_range(start=data_min, end=data_max, freq='D')
 # Criar o DataFrame com as colunas desejadas
 dCalendario = pd.DataFrame({
     'data': datas,
